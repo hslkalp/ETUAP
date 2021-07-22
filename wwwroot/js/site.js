fd70cfb975814cc4bfb4e856166c439b64c4e593
Alpgiray HAŞLAK <hslkalp@gmail.com>
2021-07-22 22:14:13 +0300

    Init

---
+++ wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+// * Menu action
+const menuBtn = document.getElementById('nav-cta');
+const nav = document.getElementById('main-nav');
+menuBtn.addEventListener('click', () => {
+	nav.classList.toggle('menu-active');
+});
